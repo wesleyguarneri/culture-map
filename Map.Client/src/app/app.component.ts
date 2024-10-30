371fd16b3e145f45db517e2ae3a41c581df23eab
wlg223 <wesguarneri@gmail.com>
2024-10-30 19:45:16 -0400

    adding arcgis files

--- Map.Client/src/app/app.component.ts
+++ Map.Client/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MapComponent } from './components/map/map.component';
+import { defineCustomElements } from "@arcgis/map-components/dist/loader";
+import { ComponentLibraryModule } from '@arcgis/map-components-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MapComponent, ComponentLibraryModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'culture-map';
+
+  arcgisViewReadyChange(event: any) {
+    console.log("MapView ready", event);
+  }
+
+  ngOnInit(): void {
+    defineCustomElements(window, { resourcesUrl: "https://js.arcgis.com/map-components/4.30/assets" });
+  }
 }
