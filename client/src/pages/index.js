010852ffca31980bca2ee611e3b06436d04e442a
wlg223 <wesguarneri@gmail.com>
2025-01-01 13:15:29 -0500

    adding country overlay

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,16 +1,47 @@
 import Head from 'next/head';
-
 import Layout from '@components/Layout';
 import Section from '@components/Section';
 import Container from '@components/Container';
 import Map from '@components/Map';
-import Button from '@components/Button';
-
 import styles from '@styles/Home.module.scss';
+import { GeoJSON } from 'react-leaflet';
 
-const DEFAULT_CENTER = [38.907132, -77.036546]
+const DEFAULT_CENTER = [38.907132, -77.036546];
+
+const geoJsonData = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      properties: { name: 'United States of America'},
+      geometry: {
+        type: 'Polygon',
+        coordinates: [
+          [
+            [-125.001650, 24.949300],
+            [-66.932640, 24.949300],
+            [-66.932640, 49.590400],
+            [-125.001650, 49.590400],
+            [-125.001650, 24.949300]
+          ]
+        ]
+      }
+    },
+  ],
+};
 
 export default function Home() {
+  const onCountryClick = (event) => {
+    const countryName = event.target.feature.properties.name;
+    alert(`You clicked on: ${countryName}`);
+  };
+
+  const onEachFeature = (feature, layer) => {
+    layer.on({
+      click: onCountryClick,
+    });
+  };
+
   return (
     <Layout>
       <Head>
@@ -23,19 +54,15 @@ export default function Home() {
             {({ TileLayer, Marker, Popup }) => (
               <>
                 <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  url="https://api.maptiler.com/maps/openstreetmap/{z}/{x}/{y}.jpg?key=bsDT2UzO0Fp8DBbkmOyZ"
                   attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 />
-                <Marker position={DEFAULT_CENTER}>
-                  <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                  </Popup>
-                </Marker>
+                <GeoJSON data={geoJsonData} onEachFeature={onEachFeature} />
               </>
             )}
           </Map>
         </Container>
       </Section>
     </Layout>
-  )
+  );
 }
