5dd4ba13836fa18006bbbb8a9c6bd7a28f16b63a
wlg223 <wesguarneri@gmail.com>
2025-05-10 23:28:21 -0400

    updating scraper

--- goodreads-scraper.py
+++ goodreads-scraper.py
@@ -44,23 +44,55 @@ class GoodreadsScraper():
 
     def get_books_from_list(self):
         list_url = "https://www.goodreads.com/list/show/1339.Best_British_and_Irish_Literature"
-        max_results = 5
+        country = "GBR"
+        max_results = 1
 
         try:
             self.driver.get(list_url)
             
             for i in range(max_results):
                 time.sleep(0.25)
-                book_listing = self.driver.find_element(By.CSS_SELECTOR,'#all_votes > table > tbody > tr:nth-child({}) > td:nth-child(3) > a > span'.format(i+1))
-                book_listing.click()
+                book_listing_button = self.driver.find_element(By.CSS_SELECTOR,'#all_votes > table > tbody > tr:nth-child({}) > td:nth-child(3) > a > span'.format(i+1))
+                book_listing_button.click()
 
                 time.sleep(0.25)
+
+                login_button = self.driver.find_element(By.CSS_SELECTOR,'body > div.Overlay.Overlay--floating > div > div.Overlay__header > div > div > button')
+                if login_button:
+                    login_button.click()
+                    time.sleep(0.25)
 
                 title = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageTitleSection > div.BookPageTitleSection__title > h1').text
                 author = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookPageMetadataSection__contributor > h3 > div > span:nth-child(1) > a > span.ContributorLink__name').text
 
+                year = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookDetails > div > span:nth-child(1) > span > div > p:nth-child(2)').text
+                year = year.split(' ')[4]
+                description = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookPageMetadataSection__description > div > div.TruncatedContent__text.TruncatedContent__text--large > div > div > span').text
                 print("{} By {}".format(title,author))
+                print("{}".format(year))
+                print("{}".format(description))
 
+                details_button = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookDetails > div > div > button')
+                details_button.click()
+
+                isbn = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookDetails > div > span:nth-child(2) > div.BookDetails__list > span > div > dl > div:nth-child(3) > dd > div > div.TruncatedContent__text.TruncatedContent__text--small').text
+                isbn = isbn.split(' ')[0]
+
+                language = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookDetails > div > span:nth-child(2) > div.BookDetails__list > span > div > dl > div:nth-child(5) > dd > div > div.TruncatedContent__text.TruncatedContent__text--small').text
+                
+                genres = []
+                for idx in range(3):
+                    genre = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookPageMetadataSection__genres > ul > span:nth-child(1) > span:nth-child({})'.format(idx+2)).text
+                    if genre == 'Classics':
+                        idx = idx-1
+                        continue
+                    genres.append(genre)
+                
+                print(genres)
+                print(language)
+                    
+                print(isbn)
+                
                 self.driver.back()
         except Exception as e:
             logging.error(e)
@@ -70,11 +102,11 @@ class GoodreadsScraper():
         options = Options()
         options.headless = True
         service = Service(executable_path='./geckodriver')
-        # self.driver = Firefox(service=service, options=options)
+        self.driver = Firefox(service=service, options=options)
 
-        # self.get_books_from_list()
-        self.connect_to_db()
-        # self.driver.close()
+        self.get_books_from_list()
+        # self.connect_to_db()
+        self.driver.close()
 
 if __name__ == "__main__":
     l = GoodreadsScraper()
