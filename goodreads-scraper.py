296e15854fd5043cbf3424872beba85e5b69b584
wlg223 <wesguarneri@gmail.com>
2025-05-14 16:06:34 -0400

    adding upload image route

--- goodreads-scraper.py
+++ goodreads-scraper.py
@@ -17,95 +17,114 @@ class GoodreadsScraper():
             self.conn = psycopg2.connect(host="localhost", database="postgres", user="admin", password="postgres")
             self.cur = self.conn.cursor()
 
-            data = {
-                "title":"Moby-Dick",
-                "author":"Herman Melville",
-                "isbn":"9780143105954",
-                "year":"1851",
-                "genre":"Adventure, Epic, Psychological Fiction",
-                "country":"USA",
-                "description": "description",
-                "language": "english"
-            }
-            self.cur.execute("INSERT INTO books (title,author,isbn,year,genre,country,description) VALUES (%s, %s, %s,%s, %s, %s,%s)", 
-                (
-                    data["title"],
-                    data["author"],
-                    data["isbn"],
-                    data["year"],
-                    data["genre"],
-                    data["country"],
-                    data["description"],
-                    data['language']
-                ))
-            self.conn.commit()
         except Exception as e:
             logging.error(e)
 
     def get_books_from_list(self):
-        list_url = "https://www.goodreads.com/list/show/1339.Best_British_and_Irish_Literature"
-        country = "GBR"
-        max_results = 1
+        list_url = "https://www.goodreads.com/list/show/2702.Best_Canadian_Literature"
+        country = "CAN"
+        max_results = 5
 
         try:
             self.driver.get(list_url)
             
             for i in range(max_results):
-                time.sleep(0.25)
+                time.sleep(0.5)
                 book_listing_button = self.driver.find_element(By.CSS_SELECTOR,'#all_votes > table > tbody > tr:nth-child({}) > td:nth-child(3) > a > span'.format(i+1))
                 book_listing_button.click()
 
-                time.sleep(0.25)
+                time.sleep(0.5)
 
-                login_button = self.driver.find_element(By.CSS_SELECTOR,'body > div.Overlay.Overlay--floating > div > div.Overlay__header > div > div > button')
-                if login_button:
-                    login_button.click()
-                    time.sleep(0.25)
+                try:
+                    login_button = self.driver.find_element(By.CSS_SELECTOR,'body > div.Overlay.Overlay--floating > div > div.Overlay__header > div > div > button')
+                    if login_button:
+                        login_button.click()
+                        time.sleep(0.5)
+                except Exception as e:
+                    print('login error')
+                
+
 
                 title = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageTitleSection > div.BookPageTitleSection__title > h1').text
                 author = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookPageMetadataSection__contributor > h3 > div > span:nth-child(1) > a > span.ContributorLink__name').text
 
                 year = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookDetails > div > span:nth-child(1) > span > div > p:nth-child(2)').text
-                year = year.split(' ')[4]
+                year = str(year.split(' ')[4])
+
                 description = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookPageMetadataSection__description > div > div.TruncatedContent__text.TruncatedContent__text--large > div > div > span').text
-                print("{} By {}".format(title,author))
-                print("{}".format(year))
-                print("{}".format(description))
 
-                details_button = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookDetails > div > div > button')
-                details_button.click()
+                try:
+                    details_button = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookDetails > div > div > button')
+                    details_button.click()
+                    time.sleep(0.25)
+                except Exception as e:
+                    print('detail button error')
 
-                isbn = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookDetails > div > span:nth-child(2) > div.BookDetails__list > span > div > dl > div:nth-child(3) > dd > div > div.TruncatedContent__text.TruncatedContent__text--small').text
-                isbn = isbn.split(' ')[0]
+                # isbn = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookDetails > div > span:nth-child(2) > div.BookDetails__list > span > div > dl > div:nth-child(3) > dd > div > div.TruncatedContent__text.TruncatedContent__text--small').text
+                # isbn = isbn.split(' ')[0]
 
-                language = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookDetails > div > span:nth-child(2) > div.BookDetails__list > span > div > dl > div:nth-child(5) > dd > div > div.TruncatedContent__text.TruncatedContent__text--small').text
-                
-                genres = []
+                dt_elements = self.driver.find_elements(By.CSS_SELECTOR, "dl > div > dt")
+
+                language = ""
+                isbn = ""
+
+                for dt in dt_elements:
+                    if dt.text.strip() == "Language":
+                        parent_div = dt.find_element(By.XPATH, "..") 
+                        dd = parent_div.find_element(By.TAG_NAME, "dd")
+                        language = dd.text.strip()
+
+                    if dt.text.strip() == "ISBN":
+                        isbn_parent_div = dt.find_element(By.XPATH, "..") 
+                        isbn_dd = isbn_parent_div.find_element(By.TAG_NAME, "dd")
+                        isbn = isbn_dd.text.strip()
+                        isbn = isbn.split(' ')[0]
+
+
+                        
+
+                genreList = []
                 for idx in range(3):
                     genre = self.driver.find_element(By.CSS_SELECTOR,'#__next > div.PageFrame.PageFrame--siteHeaderBanner > main > div.BookPage__gridContainer > div.BookPage__rightColumn > div.BookPage__mainContent > div.BookPageMetadataSection > div.BookPageMetadataSection__genres > ul > span:nth-child(1) > span:nth-child({})'.format(idx+2)).text
                     if genre == 'Classics':
                         idx = idx-1
                         continue
-                    genres.append(genre)
+                    genreList.append(genre)
                 
+                genres = ','.join(genreList)
+
                 print(genres)
                 print(language)
-                    
                 print(isbn)
                 
                 self.driver.back()
+
+                self.cur.execute("INSERT INTO books (title,author,isbn,year,genre,country,description,language) VALUES (%s, %s, %s,%s, %s, %s,%s,%s)", 
+                (
+                    title,
+                    author,
+                    isbn,
+                    year,
+                    genres,
+                    country,
+                    description,
+                    language
+                ))
+                self.conn.commit()
         except Exception as e:
             logging.error(e)
 
 
     def run(self):
+        
         options = Options()
         options.headless = True
         service = Service(executable_path='./geckodriver')
+        self.connect_to_db()
+
         self.driver = Firefox(service=service, options=options)
 
         self.get_books_from_list()
-        # self.connect_to_db()
         self.driver.close()
 
 if __name__ == "__main__":
--- server/server/Controllers/UploadController.cs
+++ server/server/Controllers/UploadController.cs
@@ -1,48 +1,86 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Amazon;
 using Amazon.S3;
 using Amazon.S3.Model;
-namespace server.Controllers;
 
-[Route("api/upload")]
-[ApiController]
-public class UploadController : Controller
+
+namespace server.Controllers
 {
-    private readonly IAmazonS3 _s3Client;
-
-    public UploadController()
+    [Route("api/upload")]
+    [ApiController]
+    public class UploadController : Controller
     {
-        _s3Client = new AmazonS3Client(RegionEndpoint.USEast2);
-    }
+        private readonly IAmazonS3 _s3Client;
+        private const string bucketName = "bookimage-bucket";
 
-    [HttpGet("presigned-url")]
-    public IActionResult GetPreSignedUrl(string fileName)
-    {
-        string bucketName = "bookimage-bucket";
-        double timeoutDuration = 12;
-
-        var url = GeneratePreSignedURL(_s3Client, bucketName, fileName, timeoutDuration);
-        return Ok(new { url });
-    }
-
-    private string GeneratePreSignedURL(
-        IAmazonS3 client,
-        string bucketName,
-        string objectKey,
-        double duration)
-    {
-        var request = new GetPreSignedUrlRequest
+        public UploadController()
         {
-            BucketName = bucketName,
-            Key = objectKey,
-            Verb = HttpVerb.PUT,
-            Expires = DateTime.UtcNow.AddHours(duration),
-        };
+            _s3Client = new AmazonS3Client(RegionEndpoint.USEast2);
+        }
 
-        return client.GetPreSignedURL(request);
+        [HttpGet("presigned-url")]
+        public IActionResult GetPreSignedUrl(string fileName)
+        {
+            double timeoutDuration = 12;
+            var url = GeneratePreSignedURL(_s3Client, bucketName, fileName, timeoutDuration);
+            return Ok(new { url });
+        }
+
+        [HttpPost("image")]
+        public async Task<IActionResult> UploadImage(IFormFile file,[FromQuery] string isbn)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            if (string.IsNullOrWhiteSpace(isbn))
+            {
+                return BadRequest("ISBN is required.");
+            }
+
+            try
+            {
+                var s3Key = $"images/{isbn}";
+
+                var stream = file.OpenReadStream();
+                var putRequest = new PutObjectRequest
+                {
+                    InputStream = stream,
+                    BucketName = bucketName,
+                    Key = s3Key,
+                    ContentType = file.ContentType
+                };
+                
+                PutObjectResponse response2 = await _s3Client.PutObjectAsync(putRequest);
+                return Ok(new { message = "Upload successful", isbn });
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        private string GeneratePreSignedURL(
+            IAmazonS3 client,
+            string bucketName,
+            string objectKey,
+            double duration)
+        {
+            var request = new Amazon.S3.Model.GetPreSignedUrlRequest
+            {
+                BucketName = bucketName,
+                Key = objectKey,
+                Verb = Amazon.S3.HttpVerb.PUT,
+                Expires = DateTime.UtcNow.AddHours(duration),
+            };
+
+            return client.GetPreSignedURL(request);
+        }
     }
 }
